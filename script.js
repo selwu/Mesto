6ba4c1d9170822e8972723901049e6b7a01cf055
selwu <selwu@mail.ru>
2020-04-26 11:55:12 +0300

    add 10 cards

---
+++ script.js
@@ -0,0 +1,81 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+  {
+    name: 'Нургуш',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg'
+  },
+  {
+    name: 'Тулиновка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg'
+  },
+  {
+    name: 'Остров Желтухина',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg'
+  },
+  {
+    name: 'Владивосток',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg'
+  }
+];
+
+const container = document.querySelector('.places-list');
+
+
+function createCard(link, name) {
+  const cardContainer = document.createElement('div');
+  cardContainer.classList.add('place-card');
+
+  const cardImage = document.createElement('div');
+  cardImage.classList.add('place-card__image');
+  cardImage.setAttribute('style', `background-image: url(${link})`);
+
+  const buttonDelete = document.createElement('button');
+  buttonDelete.classList.add('place-card__delete-icon');
+
+  const cardDescription = document.createElement('div');
+  cardDescription.classList.add('place-card__description');
+
+  const cardName = document.createElement('h3');
+  cardName.classList.add('place-card__name');
+  cardName.textContent = name;
+
+  const buttonLike = document.createElement('button');
+  buttonLike.classList.add('place-card__like-icon');
+
+  cardContainer.appendChild(cardImage);
+  cardContainer.appendChild(cardDescription);
+  cardImage.appendChild(buttonDelete);
+  cardDescription.appendChild(cardName);
+  cardDescription.appendChild(buttonLike);
+
+  return cardContainer;
+}
+
+initialCards.forEach(function (item) {
+  container.appendChild(createCard(item.link, item.name));
+});
+
+
