e3ad54f5f39b417322520d8e6f733b11504b2444
selwu <selwu@mail.ru>
2020-05-12 20:50:02 +0300

    step 3 is done

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const buttonOpenerNewCard = document.querySelector('.user-info__button');
 const buttonOpenerEditProfile = document.querySelector('.user-info__edit');
 const cardPopup = document.querySelector('.card-popup');
 const profilePopup = document.querySelector('.profile-popup');
+const imagePopup = document.querySelector('.image-popup');
+const imagePopupBig = document.querySelector('.popup__image');
 const buttonCloserCard = cardPopup.querySelector('.popup__close');
 const buttonCloserProfile = profilePopup.querySelector('.popup__close');
-
+const buttonCloserImage = imagePopup.querySelector('.popup__close');
 const userInfoName = document.querySelector('.user-info__name');
 const userInfoJob = document.querySelector('.user-info__job');
-
 const cardForm = document.forms.new;
 const editForm = document.forms.edit;
 
@@ -22,6 +23,7 @@ function createCard(link, name) {
   const cardImage = document.createElement('div');
   cardImage.classList.add('place-card__image');
   cardImage.setAttribute('style', `background-image: url(${link})`);
+  cardImage.setAttribute('data-url', link);
 
   const buttonDelete = document.createElement('button');
   buttonDelete.classList.add('place-card__delete-icon');
@@ -101,12 +103,24 @@ function submitFormEdit(event) {
   popupToggleHandler(profilePopup)();
 }
 
+function openerHandler(event) {
+  const cardPlaceName = document.querySelector('.place-card__name');
+
+  if (event.target.classList.contains('place-card__image')) {
+    imagePopupBig.src = event.target.dataset.url;
+    imagePopupBig.alt = cardPlaceName.textContent;
+    imagePopup.classList.toggle('popup_is-opened');
+  }
+}
+
 container.addEventListener('click', likeHandler);
+container.addEventListener('click', openerHandler);
 buttonOpenerNewCard.addEventListener('click', popupToggleHandler(cardPopup));
 buttonCloserCard.addEventListener('click', popupToggleHandler(cardPopup));
 buttonOpenerEditProfile.addEventListener('click', popupToggleHandler(profilePopup));
 buttonOpenerEditProfile.addEventListener('click', originNamesHandler);
 buttonCloserProfile.addEventListener('click', popupToggleHandler(profilePopup));
+buttonCloserImage.addEventListener('click', popupToggleHandler(imagePopup));
 cardForm.addEventListener('submit', addNewCard);
 editForm.addEventListener('submit', submitFormEdit);
 container.addEventListener('click', removeHandler);
